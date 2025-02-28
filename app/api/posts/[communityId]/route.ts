ef5f2955b263e64d86ef172bc19b3f5113ab1a36
Corey Munn <michael.munn@secunetics.com>
2025-02-27 21:54:56 -0500

    check the communityId to make sure that community actually exists before fetching posts for it

--- app/api/posts/[communityId]/route.ts
+++ app/api/posts/[communityId]/route.ts
@@ -1,12 +1,27 @@
 import { prisma } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function GET(
   request: Request,
   { params }: { params: { communityId: string } }
 ) {
+  auth.protect();
   try {
     const { communityId } = params;
+    // first make sure the community exists
+    const community = await prisma.community.findUnique({
+      where: {
+        id: communityId,
+      },
+    });
+    if (!community) {
+      return NextResponse.json(
+        { error: `Community ${community} not found` },
+        { status: 404 }
+      );
+    }
+    // then fetch the posts
     const posts = await prisma.post.findMany({
       where: {
         community_id: communityId,
@@ -20,7 +35,7 @@ export async function GET(
   } catch (error) {
     console.error("Error fetching posts in /api/posts/communityId: ", error);
     return NextResponse.json(
-      { error: "Failed to fetch posts" },
+      { error: `Failed to fetch posts for ${params.communityId}` },
       { status: 500 }
     );
   }
