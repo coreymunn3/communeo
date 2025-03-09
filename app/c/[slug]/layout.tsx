52d854f4049febd6cb42d9c19e0a575de14b7c71
Corey Munn <michael.munn@secunetics.com>
2025-03-09 14:30:20 -0400

    typo name

--- app/c/[slug]/layout.tsx
+++ app/c/[slug]/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const CommunityPageLayour = ({ children }: { children: React.ReactNode }) => {
+const CommunityPageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="max-h-screen max-w-6xl mx-auto mt-12 p-4">
       <div className="container mx-auto flex flex-col space-y-2">
@@ -9,4 +9,4 @@ const CommunityPageLayour = ({ children }: { children: React.ReactNode }) => {
     </div>
   );
 };
-export default CommunityPageLayour;
+export default CommunityPageLayout;
