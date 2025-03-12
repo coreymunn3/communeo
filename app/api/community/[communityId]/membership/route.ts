abb37beca8bbad4e01cd422c6b081654ed74a598
Corey Munn <michael.munn@secunetics.com>
2025-03-12 11:10:47 -0400

    update membership api route to return if the user is a moderator or founder

--- app/api/community/[communityId]/membership/route.ts
+++ app/api/community/[communityId]/membership/route.ts
@@ -12,7 +12,7 @@ export async function GET(
     const { communityId } = params;
     const dbUser = await getDbUser();
     // first make sure the community exists
-    const community = getCommunityById(communityId);
+    const community = await getCommunityById(communityId);
     if (!community) {
       return NextResponse.json(
         { error: `Community ${community} not found` },
@@ -24,10 +24,18 @@ export async function GET(
       communityId,
       dbUser.id
     );
+    const isModerator = community.moderator_id === dbUser.id;
+    const isFounder = community.founder_id === dbUser.id;
     if (membership) {
-      return NextResponse.json({ isMember: true }, { status: 200 });
+      return NextResponse.json(
+        { isMember: true, isModerator, isFounder },
+        { status: 200 }
+      );
     } else {
-      return NextResponse.json({ isMember: false }, { status: 200 });
+      return NextResponse.json(
+        { isMember: false, isModerator, isFounder },
+        { status: 200 }
+      );
     }
   } catch (error) {
     console.error(
